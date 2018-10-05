3292d5d9429373e1a73a1f3fc9ac51f1f380ed69
byer <byer.wu@gmail.com>
2018-10-05 23:25:36 +0800

    switch firebase database

--- store/index.js
+++ store/index.js
@@ -34,50 +34,50 @@ const createStore = () => {
       //
       // 每个页面自己分别去加载数据（asyncData、fetch）
       // 还是用 nuxtServerInit 加载数据这个需要在项目中去做选择！！
-      nuxtServerInit(vuexContext, context) {
-        return new Promise((resolve, reject) => {
-          // 这里使用 store.commit 把数据放到 store 里
-          vuexContext.commit('setPosts', [
-            {
-              id: '1',
-              author: 'Byer',
-              title: 'My awesome Post(Post Pages)',
-              previewText:
-                'Nuxt.js will automatically guess the file type by its extension and use the appropriate pre-processor loader for webpack. You will still need to install the required loader if you need to use them.',
-              thumbnail:
-                'https://www.dreamhost.com/blog/wp-content/uploads/2016/08/DreamHost-Top-Tech-Trends.jpg'
-            },
-            {
-              id: '2',
-              author: 'Byer',
-              title: 'My awesome Post(Post Pages)',
-              previewText:
-                'Nuxt.js will automatically guess the file type by its extension and use the appropriate pre-processor loader for webpack. You will still need to install the required loader if you need to use them.',
-              thumbnail:
-                'https://www.dreamhost.com/blog/wp-content/uploads/2016/08/DreamHost-Top-Tech-Trends.jpg'
-            }
-          ])
+      // nuxtServerInit(vuexContext, context) {
+      //   return new Promise((resolve, reject) => {
+      //     // 这里使用 store.commit 把数据放到 store 里
+      //     vuexContext.commit('setPosts', [
+      //       {
+      //         id: '1',
+      //         author: 'Byer',
+      //         title: 'My awesome Post(Post Pages)',
+      //         previewText:
+      //           'Nuxt.js will automatically guess the file type by its extension and use the appropriate pre-processor loader for webpack. You will still need to install the required loader if you need to use them.',
+      //         thumbnail:
+      //           'https://www.dreamhost.com/blog/wp-content/uploads/2016/08/DreamHost-Top-Tech-Trends.jpg'
+      //       },
+      //       {
+      //         id: '2',
+      //         author: 'Byer',
+      //         title: 'My awesome Post(Post Pages)',
+      //         previewText:
+      //           'Nuxt.js will automatically guess the file type by its extension and use the appropriate pre-processor loader for webpack. You will still need to install the required loader if you need to use them.',
+      //         thumbnail:
+      //           'https://www.dreamhost.com/blog/wp-content/uploads/2016/08/DreamHost-Top-Tech-Trends.jpg'
+      //       }
+      //     ])
 
-          // 然后再 resolve
-          resolve()
-        })
-      },
+      //     // 然后再 resolve
+      //     resolve()
+      //   })
+      // },
 
       // 坑爹的 firebase 接口超时，所以这里使用上面的静态数据
-      // nuxtServerInit({ commit }, { req }) {
-      //   return axios
-      //     .get('https://nuxt-7de9c.firebaseio.com/posts.json')
-      //     .then(res => {
-      //       console.log('res', res)
-      //       // 因为我们需要的是 [], 接口返回的是 {}, 所有这里做下转换
-      //       const postsArray = []
-      //       for (const key in res.data) {
-      //         postsArray.push({ ...res.data[key], id: key })
-      //       }
+      nuxtServerInit({ commit }, { req }) {
+        return axios
+          .get('https://nuxt-7de9c.firebaseio.com/posts.json')
+          .then(res => {
+            console.log('res', res)
+            // 因为我们需要的是 [], 接口返回的是 {}, 所有这里做下转换
+            const postsArray = []
+            for (const key in res.data) {
+              postsArray.push({ ...res.data[key], id: key })
+            }
 
-      //       commit('setPosts', postsArray)
-      //     })
-      // },
+            commit('setPosts', postsArray)
+          })
+      },
 
       // Nuxt 不知道为什么?! 好像是服务器超时（麻痹的真他妈吭 网络超时！）
       // nuxtServerInit(vuexContext, context) {
